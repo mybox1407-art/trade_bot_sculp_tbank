37fbd0ecde0e59175457d3b46322db9945cc3980
mybox1407-art <mybox1407@gmail.com>
2026-08-19 19:29:01 +0400

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import signalRouter from "./routes/signal";
 import positionRouter from "./routes/position";
 import openRouter from "./routes/open";
+import healthRouter from "./routes/health";
 
 const router = Router();
 
 router.use("/signal", signalRouter);
 router.use("/position", positionRouter);
 router.use("/open", openRouter);
+router.use("/", healthRouter);
 
 export default router;
